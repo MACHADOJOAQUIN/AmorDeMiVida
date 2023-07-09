71b3deac72d245738e44d26e4e16083b84a8ef6b
MACHADOJOAQUIN <machadoprog@gmail.com>
2023-07-09 00:51:45 -0300

    V3 Finish

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,18 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <audio  autoPlay muted  loop id='audio'>
+     <audio controls muted loop id='audio'>
       <source src="morat.mp3" type="audio/mpeg"/>
       <source src="morat.ogg" type="audio/ogg"/>
+      <source src="morat.wav" type="audio/wav"/>
+      <source src="audio.aac" type="audio/aac"/>
     </audio>
     <div className='flex mt-[7vh]'>
       <label className="switch w-[100wh] m-auto" >
         <input type="checkbox"/>
         <span onClick={() => {
           const audio = document.getElementById('audio')
+          audio.play()
           audio.muted = !audio.muted
         }
       } className="slider"></span>
